e223851b19161c2cc4c6fe0fae6c327520e0d54e
P W <przemo2048@gmail.com>
2020-02-18 12:12:20 +0100

    add custom Dataset

--- classification_cnn.py
+++ classification_cnn.py
@@ -48,13 +48,13 @@ from torchvision import datasets, models, transforms
 from torch.utils.data.sampler import SubsetRandomSampler
 import torch.nn as nn
 import torch.nn.functional as F
-
-import os
+from torch.utils.data.dataset import Dataset
 
 def prepare_dataframes(part=False):
     df_org = pd.read_csv("data/HAM10000_metadata_params_21012020_1.csv", index_col=0) 
     from sklearn.utils import shuffle
     if part:
+        
         gb = df_org.groupby('dx')    
         df_gb = [gb.get_group(x)[:100] for x in gb.groups]
         # df_gb = [gb.get_group(x)[:100] for x in gb.groups]
@@ -190,7 +190,250 @@ def show_confusion_matrix(cm, classes):
     # plt.imshow(cm, cmap='binary')
     
     
+class DatasetFromImages(Dataset):
+    def __init__(self, csv_path, img_dir):
+        """
+        Args:
+            csv_path (string): path to csv file
+            img_path (string): path to the folder where images are
+            transform: pytorch transforms for transforms and tensor conversion
+        """
+        # Transforms
+        self.to_tensor = transforms.ToTensor()
+        # Read the csv file
+        self.data_info = pd.read_csv(csv_path)
+        self.image_arr = np.asarray(self.data_info['image_id'])
+        self.label_arr = np.asarray(self.data_info['dx'])
+        # Calculate len
+        self.data_len = len(self.label_arr)
+
+    def __getitem__(self, index):
+        # Get image name from the pandas df
+        single_image_name = self.image_arr[index]
+        # Open image
+        img_as_img = Image.open(single_image_name)
+        # Transform image to tensor
+        img_as_tensor = self.to_tensor(img_as_img)
+        # Get label
+        single_image_label = self.label_arr[index]
+        return (img_as_tensor, single_image_label)
+
+    def __len__(self):
+        return self.data_len
+    
+    
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+img_dir = 'images'
+csv_path = 'data/HAM10000_metadata.csv'
+
+dataset =  DatasetFromImages(csv_path, img_dir)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # Define your transforms for the training and testing sets
+# data_transforms = {
+#     'train': transforms.Compose([
+#         transforms.RandomRotation(30),
+#         transforms.RandomResizedCrop(224),
+#         transforms.RandomHorizontalFlip(),
+#         transforms.ToTensor(),
+#         transforms.Normalize([0.485, 0.456, 0.406], 
+#                              [0.229, 0.224, 0.225])
+#     ]),
+#     'valid': transforms.Compose([
+#         transforms.Resize(256),
+#         transforms.CenterCrop(224),
+#         transforms.ToTensor(),
+#         transforms.Normalize([0.485, 0.456, 0.406], 
+#                              [0.229, 0.224, 0.225])
+#     ])
+# }
+
+# # Load the datasets with ImageFolder
+# image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
+#                                           data_transforms[x])
+#                   for x in ['train', 'valid']}
+
+# # Using the image datasets and the trainforms, define the dataloaders
+# batch_size = 64
+# dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=batch_size,
+#                                              shuffle=True, num_workers=4)
+#               for x in ['train', 'valid']}
+
+# class_names = image_datasets['train'].classes
+
+# dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'valid']}
+# class_names = image_datasets['train'].classes
+
+# print(dataset_sizes)
+# print(device)
+
+# # Label mapping
+# with open('cat_to_name.json', 'r') as f:
+#     cat_to_name = json.load(f)
+    
+# # Run this to test the data loader
+# images, labels = next(iter(dataloaders['train']))
+# images.size()
+
+# # # Run this to test your data loader
+# images, labels = next(iter(dataloaders['train']))
+# rand_idx = np.random.randint(len(images))
+# # print(rand_idx)
+# print("label: {}, class: {}, name: {}".format(labels[rand_idx].item(),
+#                                                class_names[labels[rand_idx].item()],
+#                                                cat_to_name[class_names[labels[rand_idx].item()]]))
+
+# model_name = 'densenet' #vgg
+# if model_name == 'densenet':
+#     model = models.densenet161(pretrained=True)
+#     num_in_features = 2208
+#     print(model)
+# elif model_name == 'vgg':
+#     model = models.vgg19(pretrained=True)
+#     num_in_features = 25088
+#     print(model.classifier)
+# else:
+#     print("Unknown model, please choose 'densenet' or 'vgg'")
+    
+
+
+# # Create classifier
+# for param in model.parameters():
+#     param.requires_grad = False
+
+# def build_classifier(num_in_features, hidden_layers, num_out_features):
+   
+#     classifier = nn.Sequential()
+#     if hidden_layers == None:
+#         classifier.add_module('fc0', nn.Linear(num_in_features, 102))
+#     else:
+#         layer_sizes = zip(hidden_layers[:-1], hidden_layers[1:])
+#         classifier.add_module('fc0', nn.Linear(num_in_features, hidden_layers[0]))
+#         classifier.add_module('relu0', nn.ReLU())
+#         classifier.add_module('drop0', nn.Dropout(.6))
+#         classifier.add_module('relu1', nn.ReLU())
+#         classifier.add_module('drop1', nn.Dropout(.5))
+#         for i, (h1, h2) in enumerate(layer_sizes):
+#             classifier.add_module('fc'+str(i+1), nn.Linear(h1, h2))
+#             classifier.add_module('relu'+str(i+1), nn.ReLU())
+#             classifier.add_module('drop'+str(i+1), nn.Dropout(.5))
+#         classifier.add_module('output', nn.Linear(hidden_layers[-1], num_out_features))
+        
+#     return classifier
+
+# hidden_layers = None#[4096, 1024, 256][512, 256, 128]
+
+# classifier = build_classifier(num_in_features, hidden_layers, 102)
+# print(classifier)
+
+#  # Only train the classifier parameters, feature parameters are frozen
+# if model_name == 'densenet':
+#     model.classifier = classifier
+#     criterion = nn.CrossEntropyLoss()
+#     optimizer = optim.Adadelta(model.parameters()) # Adadelta #weight optim.Adam(model.parameters(), lr=0.001, momentum=0.9)
+#     #optimizer_conv = optim.SGD(model.parameters(), lr=0.0001, weight_decay=0.001, momentum=0.9)
+#     sched = optim.lr_scheduler.StepLR(optimizer, step_size=4)
+# elif model_name == 'vgg':
+#     model.classifier = classifier
+#     criterion = nn.NLLLoss()
+#     optimizer = optim.Adam(model.classifier.parameters(), lr=0.0001)
+#     sched = lr_scheduler.StepLR(optimizer, step_size=4, gamma=0.1)
+# else:
+#     pass
+
+# # Adapted from https://pytorch.org/tutorials/beginner/transfer_learning_tutorial.html
+# def train_model(model, criterion, optimizer, sched, num_epochs=5):
+#     since = time.time()
+
+#     best_model_wts = copy.deepcopy(model.state_dict())
+#     best_acc = 0.0
+
+#     for epoch in range(num_epochs):
+#         print('Epoch {}/{}'.format(epoch+1, num_epochs))
+#         print('-' * 10)
+
+#         # Each epoch has a training and validation phase
+#         for phase in ['train', 'valid']:
+#             if phase == 'train':
+#                 model.train()  # Set model to training mode
+#             else:
+#                 model.eval()   # Set model to evaluate mode
+
+#             running_loss = 0.0
+#             running_corrects = 0
+
+#             # Iterate over data.
+#             for inputs, labels in dataloaders[phase]:
+#                 inputs = inputs.to(device)
+#                 labels = labels.to(device)
+
+#                 # zero the parameter gradients
+#                 optimizer.zero_grad()
+
+#                 # forward
+#                 # track history if only in train
+#                 with torch.set_grad_enabled(phase == 'train'):
+#                     outputs = model(inputs)
+#                     _, preds = torch.max(outputs, 1)
+#                     loss = criterion(outputs, labels)
+
+#                     # backward + optimize only if in training phase
+#                     if phase == 'train':
+#                         #sched.step()
+#                         loss.backward()
+                        
+#                         optimizer.step()
+
+#                 # statistics
+#                 running_loss += loss.item() * inputs.size(0)
+#                 running_corrects += torch.sum(preds == labels.data)
+
+#             epoch_loss = running_loss / dataset_sizes[phase]
+#             epoch_acc = running_corrects.double() / dataset_sizes[phase]
+
+#             print('{} Loss: {:.4f} Acc: {:.4f}'.format(
+#                 phase, epoch_loss, epoch_acc))
+
+#             # deep copy the model
+#             if phase == 'valid' and epoch_acc > best_acc:
+#                 best_acc = epoch_acc
+#                 best_model_wts = copy.deepcopy(model.state_dict())
+
+#         print()
+
+#     time_elapsed = time.time() - since
+#     print('Training complete in {:.0f}m {:.0f}s'.format(
+#         time_elapsed // 60, time_elapsed % 60))
+#     print('Best val Acc: {:4f}'.format(best_acc))
+
+#     #load best model weights
+#     model.load_state_dict(best_model_wts)
+    
+#     return model
+
+# epochs = 30
+# model.to(device)
+# model = train_model(model, criterion, optimizer, sched, epochs)
+
+# # Evaluation
+
+# model.eval()
     
 
 # df_org, df = prepare_dataframes(part=True)
