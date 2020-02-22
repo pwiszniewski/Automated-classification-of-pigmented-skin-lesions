101cd021015b70edeb97ad85ed01288f2efc7eda
dell <przemo2048@gmail.com>
2020-02-22 08:45:40 +0100

    add progress bar

--- cpp/mnist.cpp
+++ cpp/mnist.cpp
@@ -99,7 +99,7 @@ void test(
                      output,
                      targets,
                      /*weight=*/{},
-                     at::Reduction::Sum)
+                     torch::Reduction::Sum)
                      .template item<float>();
     auto pred = output.argmax(1);
     correct += pred.eq(targets).sum().template item<int64_t>();
--- tools/download_data.py
+++ tools/download_data.py
@@ -1,12 +1,29 @@
 import os
-import urllib
 import sys
 import zipfile
 from pathlib import Path
 import pandas as pd
 
-def download_file(url, file_name):
-	u = urllib.request.urlretrieve(url, file_name)
+try:
+	from urllib.error import URLError
+	from urllib.request import urlretrieve
+except ImportError:
+	from urllib2 import URLError
+	from urllib import urlretrieve
+
+def report_download_progress(chunk_number, chunk_size, file_size):
+	if file_size != -1:
+		percent = min(1, (chunk_number * chunk_size) / file_size)
+		bar = '#' * int(64 * percent)
+		sys.stdout.write('\r0% |{:<64}| {}%'.format(bar, int(percent * 100)))
+
+def download_file(url, file_path):
+	try:
+		urlretrieve(url, file_path, reporthook=report_download_progress)
+	except URLError:
+		raise RuntimeError('Error downloading resource!')
+	print()
+	
 
 def convert_tab_to_csv(fname_tab, fname_csv):
   df = pd.read_csv(fname_tab,delimiter="\t")
@@ -14,14 +31,14 @@ def convert_tab_to_csv(fname_tab, fname_csv):
 
 def unzip_file(file_path, dest_path):
 	with zipfile.ZipFile(file_path, 'r') as zip_ref:
-	    zip_ref.extractall(dest_path)
+		zip_ref.extractall(dest_path)
 
 def remove_file(file_path):
   if os.path.isfile(file_path):
 	  os.remove(file_path)
 
-images_path = 'images'
-data_path = 'data'
+images_path = '../images'
+data_path = '../data'
 Path(images_path).mkdir(parents=True, exist_ok=True)
 Path(data_path).mkdir(parents=True, exist_ok=True)
 
