9cfefd83788756c44e41c1038302cd08793cff2a
dell <przemo2048@gmail.com>
2020-02-25 12:03:54 +0100

    add readInfo function

--- cpp/classification_cnn.cpp
+++ cpp/classification_cnn.cpp
@@ -1,154 +1,79 @@
+#include <opencv2/opencv.hpp>
+#include <stdint.h>
 #include <torch/torch.h>
-
-#include <cstddef>
-#include <cstdio>
+#include <fstream>
 #include <iostream>
 #include <string>
 #include <vector>
 
-// Where to find the dataset.
-const char* kDataRoot = "../data";
-
-// The batch size for training.
-const int64_t kTrainBatchSize = 64;
-
-// The batch size for testing.
-const int64_t kTestBatchSize = 1000;
-
-// The number of epochs to train.
-const int64_t kNumberOfEpochs = 10;
-
-// After how many batches to log a new update with the loss value.
-const int64_t kLogInterval = 10;
-
-struct Net : torch::nn::Module {
-  Net()
-      : conv1(torch::nn::Conv2dOptions(1, 10, /*kernel_size=*/5)),
-        conv2(torch::nn::Conv2dOptions(10, 20, /*kernel_size=*/5)),
-        fc1(320, 50),
-        fc2(50, 10) {
-    register_module("conv1", conv1);
-    register_module("conv2", conv2);
-    register_module("conv2_drop", conv2_drop);
-    register_module("fc1", fc1);
-    register_module("fc2", fc2);
-  }
-
-  torch::Tensor forward(torch::Tensor x) {
-    x = torch::relu(torch::max_pool2d(conv1->forward(x), 2));
-    x = torch::relu(
-        torch::max_pool2d(conv2_drop->forward(conv2->forward(x)), 2));
-    x = x.view({-1, 320});
-    x = torch::relu(fc1->forward(x));
-    x = torch::dropout(x, /*p=*/0.5, /*training=*/is_training());
-    x = fc2->forward(x);
-    return torch::log_softmax(x, /*dim=*/1);
-  }
-
-  torch::nn::Conv2d conv1;
-  torch::nn::Conv2d conv2;
-  torch::nn::FeatureDropout conv2_drop;
-  torch::nn::Linear fc1;
-  torch::nn::Linear fc2;
+struct Options {
+  int image_size = 224;
+  size_t train_batch_size = 8;
+  size_t test_batch_size = 200;
+  size_t iterations = 10;
+  size_t log_interval = 20;
+  // path must end in delimiter
+  std::string images_path = "../../images/";
+  std::string csv_path = "../../data/HAM10000_metadata.csv";
+  // std::string datasetPath = "./dataset/";
+  // std::string infoFilePath = "info.txt";
+  torch::DeviceType device = torch::kCPU;
 };
 
-template <typename DataLoader>
-void train(
-    int32_t epoch,
-    Net& model,
-    torch::Device device,
-    DataLoader& data_loader,
-    torch::optim::Optimizer& optimizer,
-    size_t dataset_size) {
-  model.train();
-  size_t batch_idx = 0;
-  for (auto& batch : data_loader) {
-    auto data = batch.data.to(device), targets = batch.target.to(device);
-    optimizer.zero_grad();
-    auto output = model.forward(data);
-    auto loss = torch::nll_loss(output, targets);
-    AT_ASSERT(!std::isnan(loss.template item<float>()));
-    loss.backward();
-    optimizer.step();
+static Options options;
 
-    if (batch_idx++ % kLogInterval == 0) {
-      std::printf(
-          "\rTrain Epoch: %ld [%5ld/%5ld] Loss: %.4f",
-          epoch,
-          batch_idx * batch.data.size(0),
-          dataset_size,
-          loss.template item<float>());
+using Data = std::vector<std::pair<std::string, std::string>>;
+
+std::pair<Data, Data> readInfo() {
+  Data train, test;
+
+  std::ifstream stream(options.csv_path);
+  assert(stream.is_open());
+
+  std::string path, label;
+  std::string buffer, word;
+  int nline = 0;
+  while (!stream.eof()) {
+    getline(stream, buffer, '\n');
+    if (nline == 0) {
+      nline++;
+      continue; 
     }
+    std::string temp;
+    std::stringstream ss(buffer); 
+    int nword = 0;
+    while (std::getline(ss, word, ',')) { 
+      if (nword == 1) {
+        path = options.images_path + word;
+      }
+      else if (nword == 2) {
+        label = word;
+      }
+      nword++;
+    }
+    nline++;
+    train.push_back(std::make_pair(path, label));    
+    
   }
+  stream.close();
+  for (int i = 0; i < train.size(); i++) {
+    if (i < 10 or i > 10010)
+      std::cout << i << " " << train[i].first << " " << train[i].second << std::endl;
+  }
+  
+  return std::make_pair(train, test);
 }
 
-template <typename DataLoader>
-void test(
-    Net& model,
-    torch::Device device,
-    DataLoader& data_loader,
-    size_t dataset_size) {
-  torch::NoGradGuard no_grad;
-  model.eval();
-  double test_loss = 0;
-  int32_t correct = 0;
-  for (const auto& batch : data_loader) {
-    auto data = batch.data.to(device), targets = batch.target.to(device);
-    auto output = model.forward(data);
-    test_loss += torch::nll_loss(
-                     output,
-                     targets,
-                     /*weight=*/{},
-                     torch::Reduction::Sum)
-                     .template item<float>();
-    auto pred = output.argmax(1);
-    correct += pred.eq(targets).sum().template item<int64_t>();
-  }
-
-  test_loss /= dataset_size;
-  std::printf(
-      "\nTest set: Average loss: %.4f | Accuracy: %.3f\n",
-      test_loss,
-      static_cast<double>(correct) / dataset_size);
-}
-
-auto main() -> int {
+int main() {
   torch::manual_seed(1);
 
-  torch::DeviceType device_type;
-  if (torch::cuda::is_available()) {
-    std::cout << "CUDA available! Training on GPU." << std::endl;
-    device_type = torch::kCUDA;
-  } else {
-    std::cout << "Training on CPU." << std::endl;
-    device_type = torch::kCPU;
-  }
-  torch::Device device(device_type);
+  if (torch::cuda::is_available())
+    options.device = torch::kCUDA;
+  std::cout << "Running on: "
+            << (options.device == torch::kCUDA ? "CUDA" : "CPU") << std::endl;
 
-  Net model;
-  model.to(device);
+  auto data = readInfo();
 
-  auto train_dataset = torch::data::datasets::MNIST(kDataRoot)
-                           .map(torch::data::transforms::Normalize<>(0.1307, 0.3081))
-                           .map(torch::data::transforms::Stack<>());
-  const size_t train_dataset_size = train_dataset.size().value();
-  auto train_loader =
-      torch::data::make_data_loader<torch::data::samplers::SequentialSampler>(
-          std::move(train_dataset), kTrainBatchSize);
-
-  auto test_dataset = torch::data::datasets::MNIST(
-                          kDataRoot, torch::data::datasets::MNIST::Mode::kTest)
-                          .map(torch::data::transforms::Normalize<>(0.1307, 0.3081))
-                          .map(torch::data::transforms::Stack<>());
-  const size_t test_dataset_size = test_dataset.size().value();
-  auto test_loader =
-      torch::data::make_data_loader(std::move(test_dataset), kTestBatchSize);
-
-  torch::optim::SGD optimizer(
-      model.parameters(), torch::optim::SGDOptions(0.01).momentum(0.5));
-
-  for (size_t epoch = 1; epoch <= kNumberOfEpochs; ++epoch) {
-    train(epoch, model, device, *train_loader, optimizer, train_dataset_size);
-    test(model, device, *test_loader, test_dataset_size);
-  }
+  std::cout << "END";
+  return 0;
 }
